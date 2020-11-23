9d61413fc59e731d47614e1cbd2c18e7b1c51711
yaekyoon.song <yaekyoon.song@kt.com>
2020-11-23 17:40:54 +0900

    postgreSQL 적용

--- src/main/java/com/toy/aeky/natmal/sentence/SentenceController.java
+++ src/main/java/com/toy/aeky/natmal/sentence/SentenceController.java
@@ -3,13 +3,14 @@ package com.toy.aeky.natmal.sentence;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SentenceController {
 
-//    @GetMapping("")
-//    public String hello(Model model) {
-//        model.addAttribute("data", "hello!!");
-//        return "hello";
-//    }
+    @GetMapping(value = "/hello")
+    @ResponseBody
+    public String hello(Model model) {
+        return "Hello World!";
+    }
 }
