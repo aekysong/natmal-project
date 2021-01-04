feb177da923540612388b75938be4bd4659b1fcb
yaekyoon.song <yaekyoon.song@kt.com>
2021-01-04 18:14:54 +0900

    replace

---
+++ src/main/java/com/toy/aeky/natmal/config/WebSecurityConfig.java
@@ -0,0 +1,38 @@
+package com.toy.aeky.natmal.config;
+
+import com.toy.aeky.natmal.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+@RequiredArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        super.configure(web);
+        web.ignoring().antMatchers("/css/**", "/js/**", "/img/**",
+                "/swagger-resources/**", "/swagger-ui.html", "/swagger/**");
+    } // here
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.httpBasic().disable();
+    }
+}
